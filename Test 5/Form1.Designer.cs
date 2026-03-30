795aa78f023d5e6c74fc62ceeff40b999677e136
antoinmcarthur <30565188@C0560CB0021672>
2026-03-30 11:09:01 +0100

    commit

--- Test 5/Form1.Designer.cs
+++ Test 5/Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAntoin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAntoin
+            // 
+            this.btnAntoin.Location = new System.Drawing.Point(135, 110);
+            this.btnAntoin.Name = "btnAntoin";
+            this.btnAntoin.Size = new System.Drawing.Size(75, 23);
+            this.btnAntoin.TabIndex = 0;
+            this.btnAntoin.Text = "Antoin";
+            this.btnAntoin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAntoin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnAntoin;
     }
 }
 
